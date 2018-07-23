6c5a4afda5a938c94e4a13629c3e5bcead6c4346
alexey <danilenko.a@samberi.com>
2018-07-23 10:47:11 +1000

    sad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Routes, RouterModule } from "@angular/router";
+import { MatchingComponent } from './modules/matching/matching.component';
+
+
+const appRoutes: Routes = [
+  { path: '', component: MatchingComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      {useHash: true}
+    ),
+    // MaterialModule,
+    // BrowserAnimationsModule,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
